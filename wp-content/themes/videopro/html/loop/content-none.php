754be7f392b7e1c7f47db2aa307a7055cf1bcf1b
Van <vanthuyphan@gmail.com>
2017-07-03 15:29:46 +0700

    vietnamse

--- wp-content/themes/videopro/html/loop/content-none.php
+++ wp-content/themes/videopro/html/loop/content-none.php
@@ -12,15 +12,15 @@
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( esc_html__( 'Ready to publish your first post? ', 'videopro' ).'<a href="%1$s">'.esc_html__('Get started here','videopro').'</a>.', esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p><?php printf( esc_html__( 'Sẵn sàng đăng bài đầu tiên của bạn? ', 'videopro' ).'<a href="%1$s">'.esc_html__('Get started here','videopro').'</a>.', esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'videopro' ); ?></p>
+			<p><?php esc_html_e( 'Xin lỗi. Chúng tôi không tìm thấy kết quả phù hợp với từ khóa của bạn. Xin vui lòng tìm theo từ khóa khác.', 'videopro' ); ?></p>
 
 		<?php else : ?>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'videopro' ); ?></p>
+			<p><?php esc_html_e( 'Chúng tôi không tìm thấy cái bạn đang tìm kiếm. Xin vui lòng thử chức năng tìm kiềm.', 'videopro' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php endif; ?>
